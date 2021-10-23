0ef3bea74bddf4743d186b8861c7220c348b87f7
donna <dotdashhenry@gmail.com>
2021-10-23 21:35:58 +0100

    just playing

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 //You can edit ALL of the code here
+
+//create body
 const body = document.querySelector('body');
-body.style.backgroundColor = 'green';
+body.style.backgroundColor = 'pink';
 const rootElem = document.getElementById("root");
 rootElem.style.backgroundColor ='white';
+
+//create wrapper div
+const cardWrapper = document.createElement('div');
+document.body.appendChild(cardWrapper);
+cardWrapper.style.backgroundColor = 'white';
+cardWrapper.innerText= 'Something here';
+
+//button
+const btn = document.createElement("Button");
+btn.innerHTML ="Click me";
+document.body.appendChild(btn);
+
+
+//div 
+const containerDiv = document.createElement("div");
+containerDiv.innerText="test";
+containerDiv.style.color ="red";
+document.body.appendChild(containerDiv);
+
+
 
 function setup() {
   const allEpisodes = getAllEpisodes();
