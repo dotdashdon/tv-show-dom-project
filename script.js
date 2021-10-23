40b065bff76303a39975e40ac36d402a3a990a63
donna <dotdashhenry@gmail.com>
2021-10-23 15:53:32 +0100

    backgroundColor change

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //You can edit ALL of the code here
+const body = document.querySelector('body');
+body.style.backgroundColor = 'green';
+const rootElem = document.getElementById("root");
+rootElem.style.backgroundColor ='white';
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
