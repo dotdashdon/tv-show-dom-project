3c6e9a1e7e9f4d943d8e4df65c5a5909e2d7fff7
donna <dotdashhenry@gmail.com>
2021-10-24 22:57:52 +0100

    improved card

--- script.js
+++ script.js
@@ -4,45 +4,46 @@
 const body = document.querySelector('body');
 body.style.backgroundColor = 'white';
 
-
+//episode count
 const rootElem = document.getElementById("root");
 //rootElem.style.backgroundColor ='white';
 
 //container for all episodes cards 
-const container = document.createElement ('container');
+const container = document.createElement ('div');
 container.setAttribute('class', 'container');
-document.body.appendChild(container);
+container.style.backgroundColor ='fushia';
+body.appendChild(container);
 
 //episode card - append to container 
-const cardDiv = document.createElement('div'); 
-cardDiv.setAttribute('class', 'cardContainer');
-container.appendChild(cardDiv);
+const card = document.createElement('div'); 
+card.setAttribute('class', 'card');
+container.appendChild(card);
 
 //episode title append to card 
-const episodeTitle = document.createElement('h1');
-episodeTitle.setAttribute('class' ,'episodeTitle');
-episodeTitle.innerText="Episode Title";
-cardDiv.appendChild(episodeTitle);
+const episode = document.createElement('h4');
+episode.setAttribute('class' ,'episode');
+episode.innerText="Episode";
+card.appendChild(episode);
 
 
 //season append to card 
 
-const season = document.createElement('h1');
-season.setAttribute('class', 'season');
-season.innerText = '1';
-cardDiv.appendChild(season);
+// const season = document.createElement('h1');
+// season.setAttribute('class', 'season');
+// season.innerText = '1';
+// cardDiv.appendChild(season);
 
 //episode image append to card 
 
 const image = document.createElement('img');
 image.setAttribute('class', 'image');//??
-cardDiv.appendChild(image);
+card.appendChild(image);
 
 //summary append to card 
 const summary= document.createElement('p');
 summary.setAttribute('class', 'summary');
 summary.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit porttitor condimentum. Praesent sed varius mi, nec rhoncus tellus. Fusce pretium consectetur dui a dignissim. Ut tempor orci odio, sed sollicitudin purus suscipit eu. Aenean nec aliquam urna, at ultrices ante. Phasellus ultrices massa id ante accumsan cursus. Nullam auctor magna at metus rutrum accumsan. Donec pharetra lorem in ligula posuere, vel blandit neque cursus.';
-cardDiv.appendChild(summary);
+card.appendChild(summary);
 
 //box for Title 
 
@@ -56,23 +57,23 @@ cardDiv.appendChild(summary);
 
 
 
-//create wrapper div
-const cardWrapper = document.createElement('div');
-document.body.appendChild(cardWrapper);
-cardWrapper.style.backgroundColor = 'white';
-cardWrapper.innerText= 'Something here';
+// //create wrapper div
+// const cardWrapper = document.createElement('div');
+// document.body.appendChild(cardWrapper);
+// cardWrapper.style.backgroundColor = 'white';
+// cardWrapper.innerText= 'Something here';
 
-//button
-const btn = document.createElement("Button");
-btn.innerHTML ="Click me";
-document.body.appendChild(btn);
+// //button
+// const btn = document.createElement("Button");
+// btn.innerHTML ="Click me";
+// document.body.appendChild(btn);
 
 
 //div 
-const containerDiv = document.createElement("div");
-containerDiv.innerText="test";
-containerDiv.style.color ="red";
-document.body.appendChild(containerDiv);
+// const containerDiv = document.createElement("div");
+// containerDiv.innerText="test";
+// containerDiv.style.color ="red";
+// document.body.appendChild(containerDiv);
 
 
 function setup() {
